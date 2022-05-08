2520443689169db068fae2543d83fbf88fa20136
hmichinaka <oilman.toshi@gmail.com>
2022-05-08 21:07:29 +0200

    Traffic Sources module

---
+++ TraficSources/conversion_rate.sql
@@ -0,0 +1,17 @@
+/*
+Query the list of utm_content and the numbers of sessions and orders for each.
+Then, calculate the session to order conversion rate to measure the effecticity of each campaign.
+*/
+SELECT
+	website_sessions.utm_content,
+    COUNT(DISTINCT website_sessions.website_session_id) AS sessions,
+    COUNT(DISTINCT orders.order_id) AS orders,
+	COUNT(DISTINCT orders.order_id) /COUNT(DISTINCT website_sessions.website_session_id) AS session_to_order_conv_rt 
+FROM website_sessions
+	LEFT JOIN orders 
+		ON orders.website_session_id = website_sessions.website_session_id
+GROUP BY website_sessions.utm_content
+ORDER BY sessions DESC;
+
+
+
---
+++ TraficSources/traffic_sources.sql
@@ -0,0 +1,17 @@
+/*
+Query a breakdown of the total number of sessions by UTM source, 
+campaign and referring domain. The request was made on 12.04.2012.
+*/
+SELECT utm_source,
+	utm_campaign,
+    http_referer,
+    COUNT(DISTINCT website_session_id) as number_of_sessions
+FROM website_sessions
+WHERE created_at < '2012-04-12'
+GROUP BY 
+	utm_source, 
+    utm_campaign, 
+    http_referer
+ORDER BY number_of_sessions DESC;
+
+
