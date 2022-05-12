16ded3a657255e5c7d73b3a11b240ae0afc405ca
hmichinaka <oilman.toshi@gmail.com>
2022-05-12 20:53:24 +0200

    new assignments

---
+++ TraficSources/conversion_rate3.sql
@@ -0,0 +1,15 @@
+/*
+Calculate the conversion rate of a campaign 'gsearch nonbrand' per device type
+*/
+SELECT
+	website_sessions.device_type,
+	COUNT(DISTINCT website_sessions.website_session_id) AS sessions,
+	COUNT(DISTINCT orders.order_id) AS orders,
+	COUNT(DISTINCT orders.order_id) / COUNT(DISTINCT website_sessions.website_session_id) AS session_to_order_conv_rate
+FROM website_sessions
+	LEFT JOIN orders 
+		ON website_sessions.website_session_id = orders.website_session_id
+WHERE website_sessions.created_at < '2012-05-11'
+	AND utm_source = 'gsearch'
+	AND utm_campaign = 'nonbrand'
+GROUP BY device_type;
